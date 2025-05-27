8b4d4e8f666ed3f5e8e018116a09b633ac5212a0
Adise <adise@wyze.co.il>
2025-05-27 13:02:06 -0400

    test tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,9 @@ const HomePage = () => {
   return (
     <>
       <h1>Welcome to the Home Page</h1>
-      <p>This is the main content of the home page.</p>
+      <p className="text-3xl font-bold underline">
+        This is the main content of the home page.
+      </p>
     </>
   );
 };
