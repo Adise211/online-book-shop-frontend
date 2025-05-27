95252f0edfc95df12d55f85ee30047e4d8c811d9
Adise <adise@wyze.co.il>
2025-05-26 22:16:04 -0400

    implement ErrorPage component to handle and display error messages

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,34 @@
+import { useRouteError } from "react-router-dom";
+
+/**
+ * ErrorPage component to handle errors in the application.
+ * It uses the `useRouteError` hook from react-router-dom to access the error object.
+ * Displays a generic error message along with the specific error message if available.
+ */
+
 const ErrorPage = () => {
-  return <></>;
+  const error = useRouteError();
+  console.error(error);
+
+  const errorMessage = () => {
+    let msg = "Unknown Status";
+    if (error instanceof Error && "statusText" in error) {
+      msg = (error.statusText || error.message) as string;
+    }
+    return msg;
+  };
+
+  return (
+    <>
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{errorMessage()}</i>
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
