a4cbfd1dbdd008bd4866ce99ceeb1cb81867e4d6
Adise <adise@wyze.co.il>
2025-05-27 20:34:06 -0400

    add FullList component and integrate it into HomePage

---
+++ src/components/FullList.tsx
@@ -0,0 +1,11 @@
+const FullList: React.FC = () => {
+  return (
+    <div className="full-list">
+      <div className="list-wrapper w-200">
+        <p className="text-2xl">Full list</p>
+      </div>
+    </div>
+  );
+};
+
+export default FullList;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import Carousel from "../components/core/Carousel";
+import FullList from "../components/FullList";
 import MinialList from "../components/MinimalList";
 
 const HomePage = () => {
@@ -7,6 +8,8 @@ const HomePage = () => {
       <Carousel autoplay={true} />
       <div className="my-8"></div>
       <MinialList />
+      <div className="my-12"></div>
+      <FullList />
     </div>
   );
 };
